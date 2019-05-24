7465e6fec13ad8b10e92832dbfd772db383fafca
Luiz Filipe <luiz.filipe.vieira@hotmail.com>
2019-05-24 15:55:13 -0300

    BoardException and defensive programming

--- src/chess/pieces/King.java
+++ src/chess/pieces/King.java
@@ -4,15 +4,15 @@ import boardgame.Board;
 import chess.ChessPiece;
 import chess.Color;
 
-public class King extends ChessPiece{
+public class King extends ChessPiece {
 
 	public King(Board board, Color color) {
 		super(board, color);
 	}
-	
+
 	@Override
 	public String toString() {
 		return "K";
 	}
-	
+
 }
